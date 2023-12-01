c7112996baa25528ab63904f082f09d8f5e71b37
AmRo <VirtualEmailBox045@Gmail.Com>
2023-12-01 08:42:25 +0330

    Add API URL config for outline sever

---
+++ config/outline.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | API URL
+    |--------------------------------------------------------------------------
+    |
+    | This configuration specifies the management API URL for an Outline server.
+    |
+    */
+
+    'api_url' => env('OUTLINE_API_URL'),
+
+];
+
