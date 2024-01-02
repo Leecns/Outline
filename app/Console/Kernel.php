8466dbcefee91a4b571ae4ed61a5ea36ffd16b4c
AmRo <VirtualEmailBox045@Gmail.Com>
2024-01-02 23:00:59 +0330

    Add instructions for scheduler

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->job(DisableExpiredAccessKeys::class)->everyFiveSeconds();
+        $schedule->job(DisableExpiredAccessKeys::class)->everyFiveMinutes();
     }
 
     /**
@@ -21,7 +21,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands(): void
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
